194152e1e841730fb70bb54d8e0816693f7ab57f
NormanLiana <liananorman@pursuit.org>
2019-11-05 18:26:02 -0500

    adds 2 UI Objects, label and image view

--- FQ-Map-Project/Views/VenueImageCVCell.swift
+++ FQ-Map-Project/Views/VenueImageCVCell.swift
@@ -10,4 +10,17 @@ import UIKit
 
 class VenueImageCVCell: UICollectionViewCell {
     
+    // MARK: - UI Objects
+    lazy var venueImage: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .systemPink
+       return iv
+    }()
+    
+    lazy var venueNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1)
+        label.text = "Hello"
+        return label
+    }()
 }
