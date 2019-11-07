e27f9527ddfa0486ea816f2bbc8ce96f678a65e2
NormanLiana <liananorman@pursuit.org>
2019-11-07 16:58:53 -0500

    refactors TV to a CV and adds as a subview to view with constraints

--- FQ-Map-Project/Controllers/CollectionsVC.swift
+++ FQ-Map-Project/Controllers/CollectionsVC.swift
@@ -11,8 +11,8 @@ import UIKit
 class CollectionsVC: UIViewController {
     
     // MARK: UI Objects
-    lazy var listVenueTableView: UITableView = {
-        let tv = UITableView()
+    lazy var collectionsCV: UICollectionView = {
+        let tv = UICollectionView()
         tv.backgroundColor = .white
         // MARK: - TODO register cell
         return tv
@@ -21,7 +21,20 @@ class CollectionsVC: UIViewController {
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubViews()
+        constrainTableView()
+    }
+    
+    // MARK: - Private Methods
+    private func addSubViews() {
+        view.addSubview(collectionsCV)
+    }
+    
+    // MARK: - Constraint Methods
+    private func constrainTableView() {
+        collectionsCV.translatesAutoresizingMaskIntoConstraints = false
         
+        [collectionsCV.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor), collectionsCV.leadingAnchor.constraint(equalTo: view.leadingAnchor), collectionsCV.trailingAnchor.constraint(equalTo: view.trailingAnchor), collectionsCV.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)].forEach({$0.isActive = true})
     }
     
 
