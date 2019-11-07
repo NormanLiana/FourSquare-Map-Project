54277dcb6743646519b09394aae69c569bf94a2a
NormanLiana <liananorman@pursuit.org>
2019-11-07 17:20:26 -0500

    adds UI Objects img view and label

---
+++ FQ-Map-Project/Views/CollectionsCVCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionsCVCell.swift
+//  FQ-Map-Project
+//
+//  Created by Liana Norman on 11/7/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class CollectionsCVCell: UICollectionViewCell {
+    
+    // MARK: - UI Objects
+    lazy var venueImage: UIImageView = {
+        let img = UIImageView()
+        return img
+    }()
+    
+    lazy var collectionNameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+}
