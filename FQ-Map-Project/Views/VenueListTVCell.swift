bcc3319c19a3dba55d0bbe0e52c9d61944224152
NormanLiana <liananorman@pursuit.org>
2019-11-08 16:47:21 -0500

    adds cocoa file for TV cell to Views folder

---
+++ FQ-Map-Project/Views/VenueListTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  VenueListTVCell.swift
+//  FQ-Map-Project
+//
+//  Created by Liana Norman on 11/8/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class VenueListTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
