223d26d6141633114a72dfa1ffd24bcb53434ebd
NormanLiana <liananorman@pursuit.org>
2019-11-08 15:48:41 -0500

    adds new cocoa file for VenueListVC in Controllers folder

---
+++ FQ-Map-Project/Controllers/VenueListVC.swift
@@ -0,0 +1,18 @@
+//
+//  VenueListVC.swift
+//  FQ-Map-Project
+//
+//  Created by Liana Norman on 11/8/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class VenueListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
