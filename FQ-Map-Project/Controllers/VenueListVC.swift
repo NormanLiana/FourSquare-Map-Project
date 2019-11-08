a7e6759b35c0b268d2b92b6cf25e99f290271e2f
NormanLiana <liananorman@pursuit.org>
2019-11-08 16:43:26 -0500

    adds constraints for TV and calls that method in view did load as well as adding TV as a subview of the view

--- FQ-Map-Project/Controllers/VenueListVC.swift
+++ FQ-Map-Project/Controllers/VenueListVC.swift
@@ -17,8 +17,22 @@ class VenueListVC: UIViewController {
         return tv
     }()
 
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubViews()
+    }
+    
+    // MARK: - Private Methods
+    private func addSubViews() {
+        view.addSubview(venueTVList)
+    }
+    
+    // MARK: - Constriant Methods
+    private func constrainTableView() {
+        venueTVList.translatesAutoresizingMaskIntoConstraints = false
+        
+        [venueTVList.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor), venueTVList.leadingAnchor.constraint(equalTo: view.leadingAnchor), venueTVList.trailingAnchor.constraint(equalTo: view.trailingAnchor), venueTVList.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)].forEach({$0.isActive = true})
     }
     
 
