c0ce3cad0f5a71ec7f5a39e35c677348e11c2bd8
NormanLiana <liananorman@pursuit.org>
2019-11-13 13:42:37 -0500

    removes unneccasary structs, coding keys and properties from model

--- FQ-Map-Project/Models/VenueModel.swift
+++ FQ-Map-Project/Models/VenueModel.swift
@@ -7,10 +7,11 @@
 //
 
 import Foundation
+import MapKit
+import CoreLocation
 
 // MARK: - Welcome
 struct Welcome: Codable {
-    let meta: Meta
     let response: Response
     
     static func decodeVenues(from data: Data) -> [Venue]? {
@@ -24,21 +25,9 @@ struct Welcome: Codable {
     }
 }
 
-// MARK: - Meta
-struct Meta: Codable {
-    let code: Int
-    let requestID: String
-
-    enum CodingKeys: String, CodingKey {
-        case code
-        case requestID = "requestId"
-    }
-}
-
 // MARK: - Response
 struct Response: Codable {
     let venues: [Venue]
-    let confident: Bool
 }
 
 // MARK: - Venue
@@ -46,34 +35,17 @@ struct Venue: Codable {
     let id, name: String
     let location: Location
     let categories: [Category]
-    let referralID: ReferralID
     let hasPerk: Bool
     let venuePage: VenuePage?
 
-    enum CodingKeys: String, CodingKey {
-        case id, name, location, categories
-        case referralID = "referralId"
-        case hasPerk, venuePage
-    }
 }
 
 // MARK: - Category
 struct Category: Codable {
     let id, name, pluralName, shortName: String
-    let icon: Icon
     let primary: Bool
 }
 
-// MARK: - Icon
-struct Icon: Codable {
-    let iconPrefix: String
-    let suffix: Suffix
-
-    enum CodingKeys: String, CodingKey {
-        case iconPrefix = "prefix"
-        case suffix
-    }
-}
 
 enum Suffix: String, Codable {
     case png = ".png"
@@ -123,9 +95,6 @@ enum State: String, Codable {
     case ny = "NY"
 }
 
-enum ReferralID: String, Codable {
-    case v1572896320 = "v-1572896320"
-}
 
 // MARK: - VenuePage
 struct VenuePage: Codable {
