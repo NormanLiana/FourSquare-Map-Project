a262be197c5f4fdd9908085cba7c18c983b2dca6
NormanLiana <liananorman@pursuit.org>
2019-11-15 21:49:44 -0500

    adds UI object text field

--- FQ-Map-Project/Controllers/CreateCollectionVC.swift
+++ FQ-Map-Project/Controllers/CreateCollectionVC.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class CreateCollectionVC: UIViewController {
+    
+    // MARK: - UI Objects
+    lazy var collectionNameTextField: UITextField = {
+        let tf = UITextField()
+        tf.backgroundColor = .white
+        tf.textColor = .black
+        return tf
+    }()
 
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
