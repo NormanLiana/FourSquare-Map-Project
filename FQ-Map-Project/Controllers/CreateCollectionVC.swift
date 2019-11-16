38f4eecf0259893dee45a1486e889f3a460583bf
NormanLiana <liananorman@pursuit.org>
2019-11-15 23:32:24 -0500

    adds constraints to text field, adds as a subview to the view and defines text alignment and placeholder to that object, sets view's bkgrnd color to white

--- FQ-Map-Project/Controllers/CreateCollectionVC.swift
+++ FQ-Map-Project/Controllers/CreateCollectionVC.swift
@@ -15,15 +15,34 @@ class CreateCollectionVC: UIViewController {
         let tf = UITextField()
         tf.backgroundColor = .white
         tf.textColor = .black
+        tf.textAlignment = .center
+        tf.placeholder = "Enter new collection title"
         return tf
     }()
 
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpVCViews()
+        addSubViews()
+        constrainTextField()
     }
     
+    // MARK: - Private Methods
+    private func setUpVCViews() {
+        view.backgroundColor = .gray
+    }
+    
+    private func addSubViews() {
+        view.addSubview(collectionNameTextField)
+    }
+    
+    // MARK: - Constraint Methods
+    private func constrainTextField() {
+        collectionNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        [collectionNameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor), collectionNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor), collectionNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor), collectionNameTextField.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1)].forEach({$0.isActive = true})
+    }
     
 
 }
