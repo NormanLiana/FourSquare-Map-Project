e5dd24f7c2acf264d1be29fc664a3cfec86f58c8
NormanLiana <liananorman@pursuit.org>
2019-11-17 09:53:02 -0500

    updates name of persist file from helper to manager and builds the manager

--- FQ-Map-Project/Persistence/PersistenceHelper.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PersistenceHelper.swift
-//  FQ-Map-Project
-//
-//  Created by Liana Norman on 11/17/19.
-//  Copyright © 2019 Liana Norman. All rights reserved.
-//
-
-import Foundation
---
+++ FQ-Map-Project/Persistence/PersistenceManager.swift
@@ -0,0 +1,56 @@
+//
+//  PersistenceHelper.swift
+//  FQ-Map-Project
+//
+//  Created by Liana Norman on 11/17/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import Foundation
+
+struct PersistenceManager<T: Codable> {
+    
+    func getObjects() throws -> [T] {
+        guard let data = FileManager.default.contents(atPath: url.path) else {
+            return []
+        }
+        return try PropertyListDecoder().decode([T].self, from: data)
+    }
+    
+    func save(newElement: T) throws {
+        var elements = try getObjects()
+        elements.append(newElement)
+        let serializedData = try PropertyListEncoder().encode(elements)
+        try serializedData.write(to: url, options: Data.WritingOptions.atomic)
+    }
+    
+    func delete(index: Int) throws {
+        var allObjects = try getObjects()
+        allObjects.remove(at: index)
+        let serializedData = try PropertyListEncoder().encode(allObjects)
+        try serializedData.write(to: url, options: Data.WritingOptions.atomic)
+    }
+    
+    func edit(newElement: T, index: Int) throws {
+        var allObjects = try getObjects()
+        allObjects[index] = newElement
+        let serializedData = try PropertyListEncoder().encode(allObjects)
+        try serializedData.write(to: url, options: Data.WritingOptions.atomic)
+    }
+    
+    init(fileName: String){
+        self.fileName = fileName
+    }
+    
+    private let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    
+    private func filePathFromDocumentsDirectory(name: String) -> URL {
+        return documentsDirectory.appendingPathComponent(name)
+    }
+    
+    private let fileName: String
+    
+    private var url: URL {
+        return filePathFromDocumentsDirectory(name: fileName)
+    }
+}
