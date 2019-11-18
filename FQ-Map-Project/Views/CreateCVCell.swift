00ace9d0197e03afb229f93c98112ec388b054e4
NormanLiana <liananorman@pursuit.org>
2019-11-17 22:33:36 -0500

    adds cocoa file for Create CV Cell in Views folder

---
+++ FQ-Map-Project/Views/CreateCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  CreateCVCell.swift
+//  FQ-Map-Project
+//
+//  Created by Liana Norman on 11/17/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class CreateCVCell: UICollectionViewCell {
+    
+}
