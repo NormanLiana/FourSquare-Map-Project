e86fc4293aeb94e616faf939f9cce857e5bf1da6
NormanLiana <liananorman@pursuit.org>
2019-11-19 13:41:36 -0500

    builds API client for venue image network call

--- FQ-Map-Project/Networking/VenueImageAPIClient.swift
+++ FQ-Map-Project/Networking/VenueImageAPIClient.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class VenueImageAPIClient {
+
+    static let shared = VenueImageAPIClient()
+
+    func getVenueImages(venueID: Venue, completionHandler: @escaping (Result<[VenueImage], AppError>) -> () ) {
+
+        let urlStr = "https://api.foursquare.com/v2/venues/\(venueID)/photos?client_id=\(Secret.clientID)&client_secret=\(Secret.clientKey)&v=20180323&limit=2"
+        
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        
+        NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { (result) in
+            switch result {
+            case .failure(let error):
+                completionHandler(.failure(error))
+            case .success(let data):
+                do {
+                    let venueImage = try JSONDecoder().decode(ImageResponseWrapper.self, from: data)
+                    completionHandler(.success(venueImage.response.photos.items))
+                } catch {
+                    completionHandler(.failure(.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+    }
+}
