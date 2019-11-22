a2528119cafe8dd0c7906726f13de42588e855a8
NormanLiana <liananorman@pursuit.org>
2019-11-21 22:37:06 -0500

    adds commented out code for CV datasource and delegate

--- FQ-Map-Project/Controllers/CreateCollectionVC.swift
+++ FQ-Map-Project/Controllers/CreateCollectionVC.swift
@@ -88,3 +88,16 @@ class CreateCollectionVC: UIViewController {
     }
 }
 
+//extension CreateCollectionVC: UICollectionViewDelegate {}
+//
+//extension CreateCollectionVC: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        <#code#>
+//    }
+//    
+//    
+//}
